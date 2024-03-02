1300188c7f7a620b174220ee3fd88524301971e1
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-03-02 21:05:12 +0200

    feature: add header connection and style for it

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 
+import HeaderConnectionComponent from "@/components/HeaderConnectionComponent";
 import HeaderComponent from "@/components/HeaderComponent";
 import FooterComponent from "@/components/FooterComponent";
 
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <HeaderConnectionComponent />
         <HeaderComponent />
         <main>{children} </main>
         <FooterComponent />
