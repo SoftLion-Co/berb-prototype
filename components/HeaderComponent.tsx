0cc86858e5802affd6786921e83107214e8b39ae
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-03-02 17:03:14 +0200

    feature: add header, main button components and some styles for it

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,10 +1,32 @@
 import classNames from "classnames";
+import Link from "next/link";
 import s from "./HeaderComponent.module.scss";
+import MainButtonComponent from "@/components/MainButtonComponent";
+
+const NavigationItem = [
+  { text: "sanitary", href: "/sanitary" },
+  { text: "heating", href: "/heating" },
+  { text: "solar", href: "/solar" },
+  { text: "references", href: "/references" },
+  { text: "contact us", href: "/contact" },
+];
 
 const HeaderComponent = () => {
   return (
     <header className={classNames(s.header, s.container)}>
-      <h1>Header</h1>
+      <Link className={s.header__logo} href="/">
+        berb
+      </Link>
+
+      <nav className={s.header__navigation}>
+        {NavigationItem.map((item, index) => (
+          <Link className={s.header__item} key={index} href={item.href}>
+            {item.text}
+          </Link>
+        ))}
+      </nav>
+
+      <MainButtonComponent text="contact us" href="/contact" />
     </header>
   );
 };
--- components/MainButtonComponent.tsx
+++ components/MainButtonComponent.tsx
@@ -1,11 +1,17 @@
-import classNames from "classnames";
+import React, { FC } from "react";
+import Link from "next/link";
 import s from "./MainButtonComponent.module.scss";
 
-const MainButtonComponent = () => {
+interface ButtonProps {
+  text: string;
+  href: string;
+}
+
+const MainButtonComponent: FC<ButtonProps> = ({ text, href }) => {
   return (
-    <button>
-      <span>Main Button</span>
-    </button>
+    <Link className={s.main__button} href={href}>
+      {text}
+    </Link>
   );
 };
 
