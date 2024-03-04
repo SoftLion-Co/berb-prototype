f1030ffc3fab5cf5d3b57e86aa532852a0833779
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-03-04 12:19:59 +0200

    feature: add contact component

---
+++ app/section/contact_page/ContactSection.tsx
@@ -0,0 +1,12 @@
+import s from "./ContactSection.module.scss";
+import ContactComponent from "@/components/contact_page/ContactComponent";
+
+const ContactSection = () => {
+  return (
+    <section>
+      <ContactComponent />
+    </section>
+  );
+};
+
+export default ContactSection;
---
+++ components/contact_page/ContactComponent.tsx
@@ -0,0 +1,77 @@
+import Link from "next/link";
+import Image from "next/image";
+import classNames from "classnames";
+import s from "./ContactComponent.module.scss";
+import MainTitleComponent from "../MainTitleComponent";
+import MainButtonComponent from "../MainButtonComponent";
+
+import Geolocation from "@/images/Vector/Connection/Geolocation.svg";
+import Mail from "@/images/Vector/Connection/Mail.svg";
+import Phone from "@/images/Vector/Connection/Phone.svg";
+
+const ConnectionProps = [
+  {
+    text: "Berbstr. 1, Cologne, Germany",
+    icon: Geolocation,
+    href: "https://www.google.com/maps/search/Berbstr.+1,+Cologne,+Germany/@50.957239,6.6376888,10z/data=!3m1!4b1?entry=ttu",
+    target: "_blank",
+  },
+  { text: "berb@gmail.com", icon: Mail, href: "mailto:berb@gmail.com" },
+  { text: "0221/12 12 23 15", icon: Phone, href: "tel:+022112122315" },
+];
+
+const ContactComponent = () => {
+  return (
+    <div className={s.box}>
+      <div className={s.background}>
+        <div className={classNames(s.container, s.contact)}>
+          <div className={s.contact__container}>
+            <MainTitleComponent
+              className={s.contact__title}
+              title="CONTACT"
+              color="white"
+            />
+            <address className={s.contact__address}>
+              {ConnectionProps.map((item, index) => (
+                <Link
+                  className={s.contact__link}
+                  key={index}
+                  href={item.href}
+                  target={item.target}
+                >
+                  <Image
+                    className={s.contact__icon}
+                    src={item.icon}
+                    alt={item.text}
+                  />
+                  {item.text}
+                </Link>
+              ))}
+            </address>
+          </div>
+          <div className={s.contact__container}>
+            <p className={s.contact__content}>
+              <span className={s.contact__text}>
+                Broken pipe, defective heating or installation of a new
+                washbasin - we come and help you out.
+              </span>
+              <span className={s.contact__text}>
+                Our 24h emergency service will be happy to assist you!
+              </span>
+            </p>
+
+            <MainButtonComponent
+              className={s.contact__button}
+              text="Call 24 hours emergency number"
+              href=""
+              background="white"
+              color="black"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactComponent;
